25ee87c485aef46b82d74649ee408b522a79188e
Dave86dev <davidochando1986@gmail.com>
2022-05-16 13:43:34 +0200

    Primeros pasos en JavaScript

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+// // Esto es un comentario de una sola línea
+
+// /* Esto es un comentario
+//     que me permite escribir
+//         en varias líneas...
+// */
+
+//  let variable1 = 100;
+
+//  console.log("Hola, empezamos... en este caso... variable1 vale.... : ",variable1);
+
+//  variable1 = 50;
+
+//  console.log("ahora vale.... ",variable1);
+
+//  let alumno1 = "Marc";  //En este caso, alumno1 es un string que tiene como valor Marc
+
+//  let precio = 557;     //La variable precio contiene un número entero (integer)
+
+
+function Resta (numero1,numero2) {
+
+    //Dentro de las llaves, es donde hacemos la función
+    let resultado = numero1 - numero2;
+
+    return resultado;
+
+};
+
+let muestra = Resta(100,49);
+
+console.log(muestra);
